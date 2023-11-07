4cd306e1aff01a252b28945f16c5535473f29f9c
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-07 16:49:46 +0700

    update

---
+++ index.js
@@ -0,0 +1,4 @@
+// Challenge: Manipulating Attributes Practice
+// มาฝึกทำงานกับ DOM element attributes กัน จากไฟล์ html โปรดเลือก image element และ เปลี่ยน source เป็น url: https://devsprouthosting.com/images/chicken.jpg
+
+document.querySelector('img').setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg')
